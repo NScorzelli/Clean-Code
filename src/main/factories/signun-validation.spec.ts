540fdd828d465c6ed36ea23b0b7a459cc7bd071c
Nícolas <nicolasscorzelli@gmail.com>
2023-05-14 00:46:09 -0300

    refactor: move requiredFieldValidation to the ValidationComposite

---
+++ src/main/factories/signun-validation.spec.ts
@@ -0,0 +1,17 @@
+import { RequiredFieldValitaion } from '../../presentation/helpers/validators/required-field-validation'
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+import { Validation } from '../../presentation/protocols'
+import { makeSignUpValidation } from './signup-validation'
+
+jest.mock('../../presentation/helpers/validators/validation-composite.ts')
+
+describe('SignUpValidation Factory', () => {
+  test('should call ValidationComposite with all validations', () => {
+    makeSignUpValidation()
+    const validations: Validation[] = []
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValitaion(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -6,6 +6,7 @@ import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -13,7 +14,8 @@ export const makeSignUpController = (): Controller => {
   const encrypter = new BcryptAdapter(salt)
   const mongoRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(encrypter, mongoRepository)
-  const signUpController = new SignUpController(emailValidator, addAccount)
+  const validationComposite = makeSignUpValidation()
+  const signUpController = new SignUpController(emailValidator, addAccount, validationComposite)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
