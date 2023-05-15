c6dda0520f922ddb91c2f887ab4be8a5bd2bd804
Nícolas <nicolasscorzelli@gmail.com>
2023-05-15 16:39:38 -0300

    refactor: move data layer interfaces to folder

--- src/domain/usecases/authentication/db-authentication.spec.ts
+++ src/domain/usecases/authentication/db-authentication.spec.ts
@@ -1,4 +1,4 @@
-import { LoadAccountByEmailRepository } from '../../../data/protocols/load-account-by-email-repository'
+import { LoadAccountByEmailRepository } from '../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../models/account'
 import { AuthenticationModel } from '../authentication'
 import { DbAuthentication } from './db-authentication'
@@ -46,5 +46,13 @@ describe('DbAuthentication Usecase', () => {
     await sut.auth(makeFakeAuthentication())
 
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('should throw if LoadAccountByEmailRepository throws', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+    const promise = sut.auth(makeFakeAuthentication())
+
+    await expect(promise).rejects.toThrow()
   })
 })
