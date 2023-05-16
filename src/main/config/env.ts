9345b3ccbdc6d4a4be11c1843748a7c28ee6e6a4
Nícolas <nicolasscorzelli@gmail.com>
2023-05-16 08:50:02 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-node-api',
-  port: process.env.PORT || 3212
+  port: process.env.PORT || 3212,
+  jwtSecret: process.env.JWT_SECRET || 't54sr#6$%45'
 }
